1749a98ad8ca8802c488a41f4d8e32e981ad78e3
poorviacharya44 <poorviacharya72@gmail.com>
2020-12-03 22:39:37 +0530

    lab programs

--- Lab Program 3/Lab program(script)-3.js
+++ Lab Program 3/Lab program(script)-3.js
@@ -10,9 +10,9 @@ function startAnim(){
 function incr() {
     text_size += 1;
     text.innerHTML= "TEXT GROWING ..."+ text_size +"pt";
-    text.style.fontsize= text_size + "pt";
+    text.style.fontSize= text_size + "pt";
     text.style.color="red";
-    if (text_size> 49){
+    if (text_size > 49){
         window.clearInterval(timeout1);
         timeout2= window.setInterval("decr()", 100);
     }
@@ -21,7 +21,7 @@ function incr() {
 function decr() {
     text_size -= 1;
     text.innerHTML= "TEXT SHRINKING..."+ text_size +"pt";
-    text.style.fontsize= text_size + "pt";
+    text.style.fontSize= text_size + "pt";
     text.style.color="blue";
     if (text_size < 6){
         window.clearInterval(timeout2);
--- Lab Program 4/Lab program(script)-4.js
+++ Lab Program 4/Lab program(script)-4.js
@@ -1,8 +1,8 @@
  function LMV(str){
      for(i=0;i<str.length;i++)
      {
-         if(str.charAt(i)=='a' ||str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='0' || str.charAt(i)=='u')
-         return ("Left most vowel of " + str + " is at location" + (i+1));
+         if(str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o' || str.charAt(i)=='u')
+         return ("Left most vowel of " + str + " is at location " + (i+1));
      }
      return ("No vowels found for string" + str);
 
